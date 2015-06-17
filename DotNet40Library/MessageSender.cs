881dc42d18c0455c845aa3b21a6332adec0ce882
Brian Friesen <brianfriesen@quickenloans.com>
2015-06-17 19:51:57 -0400

    Implement reflection-based workaround.

--- DotNet40Library/MessageSender.cs
+++ DotNet40Library/MessageSender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Amazon.SQS;
 using Amazon.SQS.Model;
@@ -10,6 +11,8 @@ namespace DotNet40Library
         private readonly IAmazonSQS _sqs;
         private readonly string _queueUrl;
 
+        private readonly Func<string, Task> _sendAsync;
+
         public MessageSender(string queueUrl)
             : this(new AmazonSQSClient(), queueUrl)
         {
@@ -19,24 +22,43 @@ namespace DotNet40Library
         {
             _sqs = sqs;
             _queueUrl = queueUrl;
+
+            var imageRuntimeVersion = sqs.GetType().Assembly.ImageRuntimeVersion;
+
+            if (imageRuntimeVersion.StartsWith("v2"))
+            {
+                var beginSendMessageMethod = _sqs.GetType().GetMethod("BeginSendMessage", new[] { typeof(SendMessageRequest), typeof(AsyncCallback), typeof(object) });
+                var endSendMessageMethod = _sqs.GetType().GetMethod("EndSendMessage", new[] { typeof(IAsyncResult) });
+
+                _sendAsync = message =>
+                {
+                    var sendMessageRequest = new SendMessageRequest(_queueUrl, message);
+
+                    var taskCompletionSource = new TaskCompletionSource<SendMessageResponse>();
+
+                    AsyncCallback asyncCallback =
+                        ar =>
+                        {
+                            var result = (SendMessageResponse)endSendMessageMethod.Invoke(_sqs, new object[] { ar });
+                            taskCompletionSource.SetResult(result);
+                        };
+
+                    beginSendMessageMethod.Invoke(_sqs, new object[] { sendMessageRequest, asyncCallback, null });
+
+                    return taskCompletionSource.Task;
+                };
+            }
+            else
+            {
+                var sendMessageAsyncMethod = _sqs.GetType().GetMethod("SendMessageAsync", new[] { typeof(SendMessageRequest), typeof(CancellationToken) });
+
+                _sendAsync = message => (Task)sendMessageAsyncMethod.Invoke(_sqs, new object[] { new SendMessageRequest(_queueUrl, message), default(CancellationToken) });
+            }
         }
 
         public Task SendAsync(string message)
         {
-            var sendMessageRequest = new SendMessageRequest(_queueUrl, message);
-
-            var taskCompletionSource = new TaskCompletionSource<SendMessageResponse>();
-
-            AsyncCallback asyncCallback =
-                ar =>
-                {
-                    var result = _sqs.EndSendMessage(ar);
-                    taskCompletionSource.SetResult(result);
-                };
-
-            _sqs.BeginSendMessage(sendMessageRequest, asyncCallback, null);
-
-            return taskCompletionSource.Task;
+            return _sendAsync(message);
         }
     }
 }
